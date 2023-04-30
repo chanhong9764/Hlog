91ce3bf0f21e96d93521d4e3b94ebae6aa6e6ca4
chanhong9764 <chanhong9784@naver.com>
2023-04-30 15:04:47 +0900

    [기능 수정]
    1. 폰트 추가 및 설정
    2. 헤더 구성

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import theme from 'styles/theme';
 import { ThemeProvider } from 'styled-components';
+import Header from 'components/Organisms/Header';
 
 function App() {
   const darkMode = false;
 
   return (
     <ThemeProvider theme={darkMode ? theme.darkTheme : theme.lightTheme}>
-      <div className="App">React Hi</div>
+      <div className="App">
+        <Header />
+      </div>
     </ThemeProvider>
   );
 }
